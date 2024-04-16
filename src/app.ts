dab1e3671c525233d8673472584c5c7a7e28a707
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-04-16 10:01:43 +0300

    start

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 import * as middlewares from './middlewares';
 import api from './api';
@@ -11,10 +13,25 @@ require('dotenv').config();
 
 const app = express();
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+io.on('connection', (socket) => {
+  console.log(`user ${socket.id} connected`);
+
+  socket.on('disconnect', () => {
+    console.log(`user ${socket.id} disconnected`);
+  });
+});
 
 app.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
@@ -27,4 +44,4 @@ app.use('/api/v1', api);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-export default app;
+export default httpServer;
